67fb13502c68782654b1cdc898e546ec36d245e8
MikeZ2022 <mike.z@getsprout.co>
2023-01-04 13:29:29 +1300

    update for repurchase shares

--- cypress/e2e/SproutSmokeTest/Prod/19_Add Vesting Schedule.cy.js
+++ cypress/e2e/SproutSmokeTest/Prod/19_Add Vesting Schedule.cy.js
@@ -15,7 +15,7 @@
     it('1.19 Add and Remove Vesting Schedule',()=>{
     cy.contains('Securities').click()
     cy.contains('Shares').click()
-    cy.get('[data-row-key="8551"] > .ant-table-cell-fix-right > .ant-dropdown-trigger').click()
+    cy.get('[data-row-key="8552"] > .ant-table-cell-fix-right > .ant-dropdown-trigger').click()
     cy.get('.ant-dropdown-menu > :nth-child(2) > a').click()
     cy.get('#schedule_step1 > .modal-body > .body-one > .form-horizontal > :nth-child(1) > .col-xs-7 > .form-control').select('sdsd')
     cy.get('#vesting_start_date > .ant-picker > .ant-picker-input > input').click()
@@ -26,7 +26,7 @@
     cy.get('#schedule_step3 > .modal-footer > .btn').click()
     cy.contains('sdsd').should('be.exist')
     cy.visit('/home/securities/shares')
-    cy.get('[data-row-key="8551"] > .ant-table-cell-fix-right > .ant-dropdown-trigger').click()
+    cy.get('[data-row-key="8552"] > .ant-table-cell-fix-right > .ant-dropdown-trigger').click()
     cy.get('.ant-dropdown-menu > :nth-child(2) > a').click()
     cy.get(':nth-child(2) > .form-group > .acceleration_terms_a').click()
     cy.get('#removeVestingSchedule > .modal-footer > :nth-child(2)').click()
