9c2c9629e31c6bc32725685b9047e2da3e74ff01
MikeZ2022 <mike.z@getsprout.co>
2023-01-06 18:10:57 +1300

    update for repurchase share case

---
+++ cypress/DraftTestCases/28_CN covert to shares.cy.js
@@ -0,0 +1,55 @@
+// <reference types="cypress"/>
+
+describe('SproutSmokeTest',()=>{
+    beforeEach(() => {
+        // Cypress starts out with a blank slate for each test
+        // so we must tell it to visit our website with the `cy.visit()` command.
+        // Since we want to visit the same URL at the start of all our tests,
+        // we include it in our beforeEach function so that it runs before each test
+        cy.viewport('macbook-16')
+        cy.visit('https://www.getsprout.co')
+        cy.get('#hs-eu-confirmation-button').click()
+        cy.login('mzhuang2019@gmail.com','Mike_1983')
+      })
+
+    it('1.28 Convertibles convert to shares',()=>{
+    cy.contains('Securities').click()
+    cy.contains('Convertibles').click()
+    cy.get('[data-row-key="8568"] > .ant-table-cell-fix-right > .ant-dropdown-trigger').click()
+    cy.get('.ant-dropdown-menu > :nth-child(7) > a').click()
+    cy.wait(1000)
+    cy.get(':nth-child(2) > .btn').click()
+    cy.wait(1000)
+    cy.get(':nth-child(2) > .prefer-togglebtn').click()
+    cy.get('#draft_share_class').select('Ordinary (CS)')
+    cy.get(':nth-child(3) > .prefer-togglebtn').click()
+    cy.contains('CN1-2 (Investor Y)')
+    cy.get(':nth-child(4) > .prefer-togglebtn').click()
+    cy.wait(1111)
+    cy.get('#draft_number_of_shares').type('1')
+    cy.wait(1111)
+    cy.get('#draft_cash_paid').type('1')
+    cy.get(':nth-child(5) > .prefer-togglebtn').click()
+    cy.get('.no-paddingleft > .ant-select > .ant-select-selector > .ant-select-selection-item').type('miketest{enter}')
+    cy.get(':nth-child(6) > .prefer-togglebtn').click()
+    cy.get(':nth-child(1) > .ant-picker > .ant-picker-input').click()
+    cy.wait(1000)
+    cy.contains('Today').click()
+    cy.get(':nth-child(2) > .ant-picker > .ant-picker-input').click()
+    cy.wait(1000)
+    cy.contains('Today').click()
+    cy.get(':nth-child(7) > .prefer-togglebtn').click()
+    cy.get('#cancel_step1 > .modal-body > .body-one > .form-horizontal > :nth-child(2) > div.col-xs-5 > .form-control').select('Clerical error')
+    cy.get(':nth-child(4) > .row > .col-xs-6 > .form-control').type('Test From Mike')
+    cy.get('#cancel_step1 > .modal-footer > .btn').click()
+    cy.contains('Cancel certificate').click()
+    cy.contains('Canceled').should('be.exist')
+    cy.get('[data-row-key="8548"] > .ant-table-cell-fix-right > .ant-dropdown-trigger').click()
+    cy.contains('Reverse cancellation').click()
+    cy.visit('/home/summary')
+    cy.get('.glyphicon-user').click()
+    cy.contains('Log out').click() 
+    cy.clearCookies()
+    cy.getCookies().should('be.empty')
+})
+})
